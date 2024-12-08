68c851c75cdc4f109fa290105d63aa1ce9fe4e7c
cquitra3 <cquitra3@outlook.com>
2024-12-08 10:05:34 +0800

    Add files via upload

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/27/1/","2024/10/27/2/"];function toRandomPost(){
+var posts=["2024/10/27/Unity3Djiaocheng1.1/","2024/10/27/update0.93/","2024/11/10/update0.95/","2024/11/10/xieyi1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
