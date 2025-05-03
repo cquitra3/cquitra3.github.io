678cac8e622d5fe3b4e1968ff10105ac1ea92248
cquitra <cquitra@outlook.com>
2025-05-03 16:55:38 +0800

    Add files via upload

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/27/Unity3Djiaocheng1.1/","2025/03/15/math-teaching-preface/","2024/10/27/update0.93/","2024/11/10/update0.95/","2024/12/08/update1.00/","2025/03/22/update1.21/","2025/04/12/update1.30/","2025/04/20/update1.40/","2025/04/05/math-teaching-1.1/"];function toRandomPost(){
+var posts=["2024/10/27/Unity3Djiaocheng1.1/","2025/04/05/math-teaching-1.1/","2025/03/15/math-teaching-preface/","2024/10/27/update0.93/","2024/11/10/update0.95/","2024/12/08/update1.00/","2025/03/22/update1.21/","2025/04/12/update1.30/","2025/04/20/update1.40/","2025/05/03/windows10vs11/","2025/05/03/update1.50/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
